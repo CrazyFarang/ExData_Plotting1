a3d35179c241c60f0b89d69d0bbb0d11ce5813dd
CrazyFarang <mgajdadzieva@gmail.com>
2014-05-09 17:04:09 +0300

    Final R Code; Plots with transparent background

--- Plot2.R
+++ Plot2.R
@@ -1,4 +1,8 @@
-d<-read.table("./Data/household_power_consumption.txt",header=TRUE,sep=";", stringsAsFactors=FALSE)
+fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("./data")){dir.create("./data")}  #If there is no Data directory in working directory create one
+download.file(fileUrl,"./Data/Power.zip")       #download archive file from given URL. Downloaded 6/5/2014 3:34 PM
+unzip("./Data/Power.zip")                       #unzip archive file in Data folder in working directory
+d<-read.table("./Data/household_power_consumption.txt",header=TRUE,sep=";", stringsAsFactors=FALSE)     #read the file
 dim(d)          #rows=2075259     columns=9
 dim(subset(d,d$Date=="?"))    #there are no missing values in Date column
 dim(subset(d,d$Time=="?"))    #there are no missing values in Time column
@@ -11,6 +15,7 @@ dim(data)    #rows=2880    columns=10 (9 initial + DateTime)
 #convert variables in numeric 
 data$Global_active_power<-as.numeric(data$Global_active_power)
 #plot2 save it to Plot2.png file in local repository folder in the working directory
+#NOTE: Images are done with TRANSPARENT BACKGROUND like the ones given as example in the forked repository!!!
 png(file="./ExData_Plotting1/Plot2.png",width = 480, height = 480, units = "px",bg = "transparent")
 plot(data$DateTime,data$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 dev.off()
--- Plot3.R
+++ Plot3.R
@@ -1,4 +1,8 @@
-d<-read.table("./Data/household_power_consumption.txt",header=TRUE,sep=";", stringsAsFactors=FALSE)
+fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if(!file.exists("./data")){dir.create("./data")}  #If there is no Data directory in working directory create one
+download.file(fileUrl,"./Data/Power.zip")       #download archive file from given URL. Downloaded 6/5/2014 3:34 PM
+unzip("./Data/Power.zip")                       #unzip archive file in Data folder in working directory
+d<-read.table("./Data/household_power_consumption.txt",header=TRUE,sep=";", stringsAsFactors=FALSE)     #read the file
 dim(d)          #rows=2075259     columns=9
 dim(subset(d,d$Date=="?"))    #there are no missing values in Date column
 dim(subset(d,d$Time=="?"))    #there are no missing values in Time column
@@ -13,6 +17,7 @@ data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
 data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
 data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
 #plot3 save it to Plot3.png file in local repository folder in the working directory
+#NOTE: Images are done with TRANSPARENT BACKGROUND like the ones given as example in the forked repository!!!
 png(file="./ExData_Plotting1/Plot3.png",width = 480, height = 480, units = "px",bg = "transparent")
 plot(data$DateTime,data$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="",col="black")
 lines(data$DateTime,data$Sub_metering_2,col="red")
